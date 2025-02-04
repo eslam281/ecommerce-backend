a661a241998e2213468e962cd9244e9f42a1684a
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-02-04 14:35:35 +0200

    106

--- cart/view.php
+++ cart/view.php
@@ -6,10 +6,11 @@ $userid = filterRequest('userid');
 
 $data = getAllData("cartview","cart_usersid=$userid",null,false);
 
-$stmt = $con->prepare("SELECT SUM(itemsprice), SUM(countitems) as totalcount FROM `cartview` WHERE cart_usersid=$userid");
+$stmt = $con->prepare("SELECT SUM(itemsprice), SUM(countitems) as totalcount FROM `cartview` WHERE cart_usersid=$userid
+GROUP BY cart_usersid");
 
 $stmt->execute();
     
- $datacountprice = $stmt->fetch();
+ $datacountprice = $stmt->fetch(PDO::FETCH_ASSOC);
 
  echo json_encode(array("status" => "success", "datacart" => $data,"datacountprice" => $datacountprice));

dbb5ee55ad4599c8eb67bbdb8c5a43fff316f158
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-02-05 19:58:54 +0200

    113

--- items/search.php
+++ items/search.php
@@ -4,4 +4,4 @@ include"../connect.php";
 
 $search= filterRequest('search');
 
-getAllData("items","items_name LIKE('%$search%') OR items_name_ar LIKE('%$search%')");
+getAllData("itemsview","items_name LIKE('%$search%') OR items_name_ar LIKE('%$search%')");

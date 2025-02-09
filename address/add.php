3ce4d614fe62081216e7fc2dd6d49f8ad245a1cc
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-02-09 16:46:25 +0200

    address without google map

--- address/add.php
+++ address/add.php
@@ -6,16 +6,16 @@
  $name  = filterRequest('name');
  $city    = filterRequest('city');
  $street  = filterRequest('street');
- $lat     = filterRequest('lat');
- $long    = filterRequest('long');
+//  $lat     = filterRequest('lat');
+//  $long    = filterRequest('long');
 
  $data    = array(
     "address_usersid" =>   $userid , 
     "address_name"    =>   $name , 
     "address_city"    =>   $city,
     "address_street"  =>   $street,
-    "address_lat"     =>   $lat,
-    "address_long"    =>   $long,
+   //  "address_lat"     =>   $lat,
+   //  "address_long"    =>   $long,
  );
 
  insertData("address",$data);
--- address/edit.php
+++ address/edit.php
@@ -12,7 +12,7 @@
     "address_name"    =>   $name , 
     "address_city"    =>   $city,
     "address_street"  =>   $street,
-    "address_lat"     =>   $lat,
-    "address_long"    =>   $long,
+   //  "address_lat"     =>   $lat,
+   //  "address_long"    =>   $long,
  );
  updateData("address",$data,"address_id=$addressid");

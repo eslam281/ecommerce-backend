8370806284cbb66779c72f6ef0530b4cf38d715f
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-02-14 22:14:09 +0200

    150.2

--- admin/orders/approve.php
+++ admin/orders/approve.php
@@ -9,8 +9,7 @@ $data=array(
 "orders_status" => 1
 );
 
-updateData("orders",$data,"orders_id=$orderid AND orders_status =0",false);
+updateData("orders",$data,"orders_id=$orderid AND orders_status =0");
 
-sendGCM("success","The order has been approved","users$userid","","",$accesstoken);
+sendGCM("success","The order has been approved","users$userid","","order",$accesstoken);
 
-printSuccess();

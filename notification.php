d5a334cb0983d327613cc9e7af83757f57aa76e8
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-02-15 22:08:10 +0200

    155.2

--- notification.php
+++ notification.php
@@ -3,4 +3,14 @@ include "connect.php";
 
 $userid = filterRequest('userid');
 
- getAllData("notification","notification_userid = $userid",null);
+
+ $stmt = $con->prepare("SELECT * FROM `notification` where notification_userid = $userid  ORDER by notification_datetime DESC");
+$stmt->execute();
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$count  = $stmt->rowCount();
+
+if ($count > 0) {
+    echo json_encode(array("status" => "success", "data" => $data));
+} else {
+    echo json_encode(array("status" => "failure"));
+}

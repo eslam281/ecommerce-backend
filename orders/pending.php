485f808c65dbd9d7e500f6bcc6b87158e2b85cb2
اسلام سيد <aslamsydbdalzyzbry@gmail.com>
2025-02-17 14:31:58 +0200

    160

--- orders/pending.php
+++ orders/pending.php
@@ -4,4 +4,14 @@ include"../connect.php";
 
 $userid = filterRequest("userid");
 
-getAllData("ordersview","orders_usersid = $userid");
+// getAllData("ordersview","orders_usersid = $userid");
+$stmt = $con->prepare("SELECT * FROM `ordersview` where orders_usersid = $userid  ORDER by orders_datetime DESC");
+$stmt->execute();
+$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$count  = $stmt->rowCount();
+
+if ($count > 0) {
+    echo json_encode(array("status" => "success", "data" => $data));
+} else {
+    echo json_encode(array("status" => "failure"));
+}
